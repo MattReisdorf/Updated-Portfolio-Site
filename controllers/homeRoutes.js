8cadff03ba33c4c68967ccbfbb1b048ff212f6b0
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-17 14:15:08 -0400

    making some changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
-
 router.get('/', (req, res) => {
     res.render('homepage');
 })
 
+
+//THIS IS FOR THE PHP POST, STILL NOT WORKING
+
+// router.post('/php/mail.php', (req, res) => {
+//     console.log(req.body);
+//     res.redirect('/');
+// })
 
 module.exports = router;
